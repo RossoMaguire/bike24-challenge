8dee6dffb37d588b6e918665198c3892bc1cbf09
Ross Maguire <ross.rmdesigns@gmail.com>
2022-10-07 21:58:04 +0100

    feat: fetch products in ProductSelect component

--- RossoMaguireCodingChallenge/src/components/Sections/Controls/Controls.tsx
+++ RossoMaguireCodingChallenge/src/components/Sections/Controls/Controls.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Grid, GridItem } from '@chakra-ui/react';
+import ProductSelect from './ProductSelect';
 
 const Controls: React.FC = () => {
   return (
     <Grid templateColumns='repeat(3, 1fr)' gap={6}>
       <GridItem w='100%' h='10' bg='blue.500'>
-        Dropdown
+        <ProductSelect />
       </GridItem>
       <GridItem w='100%' h='10' bg='blue.500'>
         Slider
---
+++ RossoMaguireCodingChallenge/src/components/Sections/Controls/ProductSelect.tsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+import { Select } from '@chakra-ui/react';
+
+const ProductSelect: React.FC = () => {
+  const [productData, setProductData] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch('../../../data/products.json')
+      .then((response) => response.json())
+      .then((data) => setProductData(data));
+  }, []);
+
+  return (
+    <Select placeholder='Select a Product'>
+      {productData.map((product, index) => (
+        <option key={product.id} value={`product-${index}`}>
+          {product.productName}
+        </option>
+      ))}
+    </Select>
+  );
+};
+
+export default ProductSelect;
