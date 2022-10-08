f449e4b842863738b56f7eb12baf3f950932111f
Ross Maguire <ross.rmdesigns@gmail.com>
2022-10-08 22:35:24 +0100

    feat: wiring up context with product select

--- RossoMaguireCodingChallenge/src/components/Sections/Controls/ProductSelect.tsx
+++ RossoMaguireCodingChallenge/src/components/Sections/Controls/ProductSelect.tsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react';
 import { Select } from '@chakra-ui/react';
-
+import { useCartContext } from '../../context/CartContext';
 const ProductSelect: React.FC = () => {
   const [productData, setProductData] = useState<Product[]>([]);
+
+  const { setSelectedProduct } = useCartContext();
 
   useEffect(() => {
     fetch('../../../data/products.json')
@@ -10,10 +12,21 @@ const ProductSelect: React.FC = () => {
       .then((data) => setProductData(data));
   }, []);
 
+  const handleChange = (productId: string) => {
+    const productSelected = productData.find(
+      (product) => product.id === productId
+    );
+    setSelectedProduct(productSelected!);
+  };
+
   return (
-    <Select placeholder='Select a Product' color='black'>
+    <Select
+      placeholder='Select a Product'
+      color='black'
+      onChange={(e) => handleChange(e.target.value)}
+    >
       {productData.map((product, index) => (
-        <option key={product.id} value={`product-${index}`}>
+        <option key={`product-${index}`} value={product.id}>
           {`${product.productName} || €${product.price.toString()}`}
         </option>
       ))}
--- RossoMaguireCodingChallenge/src/main.tsx
+++ RossoMaguireCodingChallenge/src/main.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
+import { CartProvider } from './components/context/CartContext';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
