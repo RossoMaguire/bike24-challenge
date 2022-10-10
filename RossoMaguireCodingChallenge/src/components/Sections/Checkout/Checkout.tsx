c088412fd3c439243975a6d7dabd6ba8db5c8193
Ross Maguire <ross.rmdesigns@gmail.com>
2022-10-10 08:52:11 +0100

    feat: adding Order Total to orders table

--- RossoMaguireCodingChallenge/src/components/Sections/Checkout/Checkout.tsx
+++ RossoMaguireCodingChallenge/src/components/Sections/Checkout/Checkout.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
-import { Flex, Box, Spacer, Button } from '@chakra-ui/react';
+import { Flex, Box, Spacer, Button, Heading } from '@chakra-ui/react';
+import { useCartContext } from '../../context/CartContext';
 
 const Checkout: React.FC = () => {
+  const { cartTotal } = useCartContext();
+
   return (
-    <Flex>
-      <Box p='4'>
-        <Button colorScheme='red'>Clear Cart</Button>
-      </Box>
-      <Spacer />
-      <Box p='4'>
-        <Button colorScheme='green'>Buy</Button>
-      </Box>
-    </Flex>
+    <>
+      <Flex>
+        <Spacer />
+        <Box p='2'>
+          <Heading size='md' color='black'>
+            {`Order Total: ${cartTotal}`}
+          </Heading>
+        </Box>
+      </Flex>
+      <Flex>
+        <Box p='4'>
+          <Button colorScheme='red'>Clear Cart</Button>
+        </Box>
+        <Spacer />
+        <Box p='4'>
+          <Button colorScheme='green'>Buy</Button>
+        </Box>
+      </Flex>
+    </>
   );
 };
 
