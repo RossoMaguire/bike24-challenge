7b73c2f2e773103018f8f30475eb01bcda9249d4
Ross Maguire <ross.rmdesigns@gmail.com>
2022-10-10 16:42:59 +0100

    feat: adding Header component with logo

--- RossoMaguireCodingChallenge/src/App.tsx
+++ RossoMaguireCodingChallenge/src/App.tsx
@@ -1,10 +1,12 @@
-import { Container, Grid, GridItem } from '@chakra-ui/react';
+import { Container, Grid, GridItem, Flex } from '@chakra-ui/react';
 import { Checkout, Controls, OrdersTable } from './components/Sections';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className='App'>
       <Container maxW='1440' color='white' py='20'>
+        <Header />
         <Controls />
         <OrdersTable />
         <Checkout />
